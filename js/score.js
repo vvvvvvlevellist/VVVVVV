a70c0765bad6279dbb413f9e483f733d3e7029dd
vvvvvvlevellist <vvvvvvlevellist@gmail.com>
2026-06-06 19:58:15 +1000

    ewsf

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = 1000 * Math.pow(0.5, (rank - 1) / 10) *
+    let score = (-30*Math.pow(rank-1, 0.76) + 500) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
