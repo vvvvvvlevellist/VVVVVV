151a039e480f950d4d535127ea53f809cb5c846d
MythicalWrath <152750475+MythicalWrath@users.noreply.github.com>
2026-06-07 20:14:08 +1000

    return roulette test

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Legacy from './pages/Legacy.js';
+import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/legacy', component: Legacy },
+    { path: '/roulette', component: Roulette },
 ];
